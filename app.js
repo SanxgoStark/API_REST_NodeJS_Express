5e2b2f408be33236bc2ff2a066a2425ed191f205
SanxgoStark <17030203@itcelaya.edu.mx>
2020-05-12 00:22:26 -0500

    Renderizado de vistas login y producto

--- app.js
+++ app.js
@@ -5,6 +5,9 @@
  * los nuevos tipos de variables
  */
 
+ // libreria para poder renderizar html del servidor al cliente
+const hbs = require('express-handlebars')
+
 /**
  * Archivo de configuracion de la aplicacion con express
  */
@@ -38,6 +41,52 @@
  // Para poder admitir peticiones con cuerpo de mensaje en formato json
  app.use(bodyParser.json())
 
+
+// configurar el motor de plantilla en express que usamos en node
+
+/**
+ * Express lo que va hacer es buscar en una carpeta llamada viewa 
+ * las plantillas que crearemos para renderizar las paginas
+ * 
+ * por defecto tomara un layaut que por defecto se comvertira en el 
+ * padre de todas las vistas
+ * 
+ * cada vista dependiendo de la ruta que nosotros eligamos renderizara una unica plantlla que se
+ * renderizara en ese unico template default que nosotros hayamos definido
+ */
+
+
+/**
+ * Los ficheros con extension .hbs usen hbs
+ * 
+ * extname: '.hbs' ---> que los ficheros 
+ * handlebars utlizen .hbs como extencion por defecto
+ * 
+ * app.set('view engine','.hbs') -----> view engine es el que se
+ * ha definido arriba como .hbs
+ */
+app.engine('.hbs',hbs({
+    defaultLayout: 'default',
+    extname: '.hbs'
+
+}))
+app.set('view engine','.hbs')
+
+// definicion que para /api se cargen las apis
  app.use('/api',api)
+
+ /**
+  * Para /login mande un res.render()
+  * 
+  * prependListener([indicamos que vista de las que estan a la altura de views que remos que renderize])
+  */
+ app.get('/login', (req,res) => {
+     res.render('login')
+ })
+
+ // para el render de product
+ app.get('/', (req, res) => {
+    res.render('product')
+  })
 
  module.exports = app 
